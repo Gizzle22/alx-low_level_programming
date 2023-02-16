572aac390bdabd9b31a802aca5c0936ced6af27b
Gizzle22 <ellelgizzle@gmai.com.com>
2023-02-16 10:11:45 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,12 +5,12 @@
  * Return:zero if no error, Non-zero value  if errors
  */
 
-int main (void)
+int main(void)
 
 {
 
-   puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-   return (0);
+return (0);
 
 }
