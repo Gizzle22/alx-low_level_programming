fa5bb1ffd3bb5140c302a47b5c1fa4da50cd4987
Gizzle22 <ellelgizzle@gmai.com.com>
2023-02-17 13:28:43 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-int num;
-/*to print all single numbers of base 10*/
-for (num = 0 ; num < 10 ; num++)
-printf("%d", num);
-prinf("\n");
+int x;
+	/*to print all single numbers of base 10,loop through them*/
+for (x = 0 ; x < 10 ; x++)
+printf("%d", x);
+printf("\n");
 return (0);
 }
